853c9f3cb618b75418411c9009d6619b3270746a
BETHWEL <bethwelk85@gmail.com>
2023-03-15 09:43:02 +0300

    done num 3

---
+++ greaternum.js
@@ -0,0 +1,11 @@
+function greaterNumber(firstnum,secondnum){
+    if (firstnum>secondnum){
+        return firstnum;
+    }
+    else{
+        return secondnum;
+    }
+    
+}
+
+console.log(greaterNumber(1,2));
